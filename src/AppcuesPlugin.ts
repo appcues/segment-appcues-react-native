cd647e384cb5d44524580fbffab19c029d5eb9c4
James Ellis <james@appcues.com>
2023-08-14 11:30:59 -0400

    ✨ Add the Appcues plugin implementation

---
+++ src/AppcuesPlugin.ts
@@ -0,0 +1,70 @@
+import {
+    DestinationPlugin,
+    PluginType,
+    TrackEventType,
+    ScreenEventType,
+    SegmentAPISettings,
+    UpdateType,
+    IdentifyEventType,
+    GroupEventType
+  } from '@segment/analytics-react-native';
+  import type { SegmentAppcuesSettings } from './types';
+  import * as Appcues from '@appcues/react-native';
+  
+export class AppcuesPlugin extends DestinationPlugin {    
+    type = PluginType.destination;
+    key = 'Appcues Mobile';
+
+    private isInitialized: boolean = false;
+  
+    async update(settings: SegmentAPISettings, _: UpdateType) {
+        if (this.isInitialized) {
+            return
+        }
+       
+        const appcuesSettings = settings.integrations[
+            this.key
+        ] as SegmentAppcuesSettings;
+  
+        if (appcuesSettings === undefined) {
+            return;
+        }
+
+        Appcues.setup(appcuesSettings.accountId, appcuesSettings.applicationId)
+            .then(() => { this.isInitialized = true; });
+    }
+  
+    identify(event: IdentifyEventType) {
+        if (this.isInitialized && event.userId != null) {
+            Appcues.identify(event.userId, event.traits);
+        }
+        return event;
+    }
+  
+    track(event: TrackEventType) {  
+        if (this.isInitialized) {
+            Appcues.track(event.event, event.properties);
+        }
+        return event;
+    }
+  
+    screen(event: ScreenEventType) {
+        if (this.isInitialized) {
+            Appcues.screen(event.name, event.properties);
+        }
+      return event;
+    }
+  
+    group(event: GroupEventType) {
+        if (this.isInitialized) {
+            Appcues.group(event.groupId, event.traits);
+        }
+        return event;
+    }
+  
+    reset(): void {
+        if (this.isInitialized) {
+            Appcues.reset()
+        }
+    }
+}
